4d3a584db4fcfc01a12185a725e0023a38f30271
Taarak Gada <taarakgada05@gmail.com>
2026-07-08 12:47:41 +0530

    feat: integrate Google Tag Manager (GTM-W5XLM4LL) script and noscript tracking tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,15 @@ export default function RootLayout({
       className={`${inter.variable} ${urbanist.variable} h-full antialiased`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W5XLM4LL');`}
+        </Script>
+
         {/* Google Analytics (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-R1GKCMFYJF"
@@ -143,6 +152,15 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col bg-white text-slate-900 selection:bg-primary/20 selection:text-primary">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W5XLM4LL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         <main className="flex-1 pt-20 pb-16 md:pb-16 pb-28">
           {children}
